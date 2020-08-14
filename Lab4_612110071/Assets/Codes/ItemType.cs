d90f011e5426cd9802e0cb94c3b3c20a683e59db
DESKTOP-325D3AA\Poom <poom_se@cmu.ac.th>
2020-08-14 16:30:29 +0700

    Lab4

---
+++ Lab4_612110071/Assets/Codes/ItemType.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType 
+{
+    SPHERE_DROP_ITEM,
+    CUBE_OBSTACLE,
+    CAPSULE_OBSTACLE,
+    CYLINDER_OBSTACLE,
+    JK_OBSTACLE,
+    BOX_OBSTACLE
+}
